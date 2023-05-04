7bb2acc7ef48f3d4b599fe8cff4570f9849ebc14
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-05-04 18:28:45 +0530

    1st page commit

--- screens/Login.js
+++ screens/Login.js
@@ -1,21 +1,50 @@
-import { View, Text, ScrollView, StyleSheet, ImageBackground, Dimensions } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, ImageBackground, Dimensions, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
-import { MainScroll, LoginTitle } from '../src/global/styles';
+import { MainView, LoginTitle, LoginButton, ButtonText, SocialLoginView, LinkText, FooterView, OrLine, OrContainer } from '../src/global/styles';
 import { TextInput } from 'react-native-paper';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Login = () => {
   return (
-    <MainScroll showsVerticalScrollIndicator={false}>
+    <MainView>
       <ImageBackground source={require('../assets/images/bg.png')} style={{height:Dimensions.get('window').height / 2.5}}></ImageBackground>
       <View style={styles.bottomView}>
         <View style={{padding: 40}}>
             <LoginTitle>Enter Your Mobile Number To Get OTP</LoginTitle>
-            <TextInput style={{marginTop: 20}} mode='outlined' activeOutlineColor='#F49F1C' placeholder='10 Digit Mobile Number' label='Mobile Number'/>
+            <TextInput left={<TextInput.Affix text="+91"/>} style={{marginTop: 20}} keyboardType='numeric' mode='outlined' activeOutlineColor='#F49F1C' placeholder='10 Digit Mobile Number' label='Mobile Number'/>
+            <LoginButton  
+              onPress={() => {}}
+            >
+              <ButtonText>Get OTP</ButtonText>
+            </LoginButton>
         </View>
+        <OrContainer>
+          <OrLine />
+            <Text style={{fontSize: 18, textAlign: 'center' }}>Or</Text>
+          <OrLine />
+        </OrContainer>
       </View>
-    </MainScroll>
-  )
-}
+      <SocialLoginView>
+        <TouchableOpacity onPress={() => {}}>
+          <FontAwesome name="envelope" size={24} color="gray" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}}>
+          <Image  source={require('../assets/images/gmail.png')} />
+        </TouchableOpacity>  
+        <TouchableOpacity onPress={() => {}}>
+          <Image source={require('../assets/images/fb.png')} />
+        </TouchableOpacity>  
+      </SocialLoginView>
+      <FooterView>
+        <Text style={{ textAlign: 'center', justifyContent: 'center' }}>By Clicking, I Accept The  
+          <TouchableOpacity><LinkText> Terms Of Service</LinkText>
+          </TouchableOpacity>  And 
+          <TouchableOpacity><LinkText>Privacy Policy</LinkText></TouchableOpacity>
+        </Text>
+      </FooterView>
+    </MainView>
+  );
+};
 
 export default Login;
 
--- src/global/styles.js
+++ src/global/styles.js
@@ -15,8 +15,9 @@ export const Title = styled.Text`
   font-weight: bold;
 `;
 
-export const MainScroll = styled.ScrollView`
+export const MainView = styled.SafeAreaView`
     flex: 1;
+    margin-top: 0;
     background-color: #fff;
 `;
 
@@ -24,5 +25,54 @@ export const LoginTitle = styled.Text`
     text-align: center;
     font-size: 16px;
     font-weight: 600;
-    font-family: 'Roboto';
 `;
+export const LoginButton = styled.TouchableOpacity`
+  height: 50px;
+  width: 100%;
+  background-color: #F49F1C;
+  padding-top: 16px;
+  align-items: center;
+  margin-top: 25px;
+  border-radius: 5px;
+`;
+
+export const ButtonText = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-weight: 600;
+`;
+ 
+export const SocialLoginView = styled.View`
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
+  bottom: 110px;
+  width: 80%;
+  margin: auto;
+`;
+
+export const FooterView = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LinkText = styled.Text`
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  text-decoration: underline;
+`;
+export const OrContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+export const OrLine = styled.View`
+  flex: 1;
+  background-color: #7B7A7A;
+  height: 1px;
+  margin-left: 20px;
+  margin-right: 20px;
+  `;
