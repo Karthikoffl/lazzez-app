1d46e4d1ab968430515078f58e9a5cb6904e50b7
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-05-05 23:26:23 +0530

    home half commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet, TextInput, FlatList, ScrollView, Pressable } from 'react-native';
 import { MainHomeView, Header, HomeLogo, LocationView, SearchView } from '../src/global/styles';
 import { Feather, AntDesign, FontAwesome } from '@expo/vector-icons';
+import { filterData } from '../src/global/Data';
+import Swiper from 'react-native-swiper';
 
 const HomeScreen = () => {
+ 
+  const [indexCheck, setIndexCheck] = useState("0");
+
   return (
     <MainHomeView>
       <Header>
@@ -27,10 +32,112 @@ const HomeScreen = () => {
           <FontAwesome name="microphone" size={24} color="#F49F1C" />
         </TouchableOpacity>  
       </SearchView>
+      <ScrollView>
+        <View style={{flex: 4, justifyContent: 'center'}}>
+          <Swiper
+            height={150}
+            pagingEnabled={true}
+            showsHorizontalScrollIndicator={false}
+            bounces={false}
+            showsPagination={true}
+            autoplay={true}
+            autoplayTimeout={2}
+            activeDotColor='#F49F1C'
+          >
+            <View style={styles.slide1}>
+              <Image 
+                source={require('../assets/images/slider-1.png')}
+                
+              />
+            </View>
+            <View style={styles.slide2}>
+              <Image 
+                source={require('../assets/images/slider-1.png')}
+                
+              />
+            </View>
+            <View style={styles.slide3}>
+              <Image 
+                source={require('../assets/images/slider-1.png')}
+                
+              />
+            </View>
+          </Swiper>
+        </View>
+        <View style={{marginLeft: 22, marginTop: 10}}>
+          <Text style={{fontWeight: 600, fontSize: 18}}>All Categories</Text>
+        </View>
+        <View style={{marginTop: 15}}>
+          <FlatList 
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            data={filterData}
+            keyExtractor={(item) => item.id}
+            extraData= {indexCheck}
+            renderItem={({item, index}) => (
+              <TouchableOpacity 
+                onPress={() => {setIndexCheck(index.id)}}
+              >
+                <View style={styles.smallCard}>
+                  <Image 
+                    style={{height: 60, width: 60, borderRadius: 30}}
+                    source={item.image}
+                  />
+                </View>
+                <View>
+                  <Text style={styles.smallCardText}>{item.name}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+        <View style={{flex: 1}}>
+          <View style={{flexDirection: 'row', marginLeft: 22, marginTop: 35, alignItems: 'center', justifyContent: 'space-between'}}>
+            <Text style={{fontWeight: 600, fontSize: 18}}>Set Menu</Text>
+            <TouchableOpacity 
+              style={{alignItems: 'center', justifyContent: 'center', marginRight: 22}}>
+                <Text style={{color: '#F49F1C', fontSize: 14, textAlign: 'center'}}>View All</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </MainHomeView>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  smallCard: {
+    borderRadius: 50,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 80,
+    width: 80,
+    marginHorizontal: 10,
+  },
+  smallCardText: {
+    fontWeight: 'bold',
+    color: '#000',
+    textAlign: 'center',
+  },
+});
