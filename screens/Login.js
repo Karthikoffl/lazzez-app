12b74a19b2e598b28f037c0f352cfd452f841298
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-05-24 18:33:33 +0530

    (2405) commit

--- screens/Login.js
+++ screens/Login.js
@@ -10,7 +10,8 @@ import {
 } from 'react-native';
 import React, { 
   useContext, 
-  useState 
+  useState,
+  useEffect 
 } from 'react';
 import { 
   MainView, 
@@ -29,50 +30,72 @@ import {
   ActivityIndicator, 
   MD2Colors 
 } from "react-native-paper";
-// import auth from '@react-native-firebase/auth';
 // import { GoogleSignin } from '@react-native-google-signin/google-signin';
+// import auth, { getAuth, FacebookAuthProvider, signInWithCredential } from '@react-native-firebase/auth';
 // import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
-
-// async function onFacebookButtonPress() {
-//   // Attempt login with permissions
-//   const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
-
-//   if (result.isCancelled) {
-//     throw 'User cancelled the login process';
-//   }
-
-//   // Once signed in, get the users AccesToken
-//   const data = await AccessToken.getCurrentAccessToken();
-
-//   if (!data) {
-//     throw 'Something went wrong obtaining access token';
-//   }
-
-//   // Create a Firebase credential with the AccessToken
-//   const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
-
-//   // Sign-in the user with the credential
-//   return auth().signInWithCredential(facebookCredential);
-// }
-
-// async function onGoogleButtonPress() {
-//   // Check if your device supports Google Play
-//   await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-//   // Get the users ID token
-//   const { idToken } = await GoogleSignin.signIn();
-
-//   // Create a Google credential with the token
-//   const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-//   // Sign-in the user with the credential
-//   return auth().signInWithCredential(googleCredential);
-// }
-
+// import { firebaseConfig } from '..App';
+// import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
 
 const Login = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { onLogin, error, isLoading } = useContext(AuthenticationContext);
+
+  // const [initializing, setInitializing] = useState(true);
+  // const [user, setUser] = useState();
+  // GoogleSignin.configure({
+  //   webClientId: '586012277192-8m04oke5sa6mslamphsaovrd6dn2dd5k.apps.googleusercontent.com',
+  // });
+  // function onAuthStateChanged(user) {
+  //   setUser(user);
+  //   if (initializing) setInitializing(false);
+  // }
+
+  // useEffect(() => {
+  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+  //   return subscriber; // unsubscribe on unmount
+  // }, []);
+
+  // const onGoogleButtonPress = async () => {
+  //   // Get the users ID token
+  //   const { idToken } = await GoogleSignin.signIn();
+  
+  //   // Create a Google credential with the token
+  //   const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+  //   // Sign-in the user with the credential
+  //   const user_sign_in = auth().signInWithCredential(googleCredential);
+  //   user_sign_in.then((user) => {
+  //     console.log(user);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   })
+  // }
+
+  // if (initializing) return null;
+
+  const onFacebookButtonPress = async () => {
+    // Attempt login with permissions
+    const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+  
+    if (result.isCancelled) {
+      throw 'User cancelled the login process';
+    }
+  
+    // Once signed in, get the users AccesToken
+    const data = await AccessToken.getCurrentAccessToken();
+  
+    if (!data) {
+      throw 'Something went wrong obtaining access token';
+    }
+  
+    // Create a Firebase credential with the AccessToken
+    const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+  
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(facebookCredential);
+  }
 
   return (
     <MainView>
@@ -125,10 +148,10 @@ const Login = ({ navigation }) => {
         {/* <TouchableOpacity onPress={() => {}}>
           <FontAwesome name="envelope" size={24} color="gray" />
         </TouchableOpacity> */}
-        <TouchableOpacity onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}>
+        <TouchableOpacity onPress={() => {}}>
           <Image  source={require('../assets/images/gmail.png')} />
         </TouchableOpacity>  
-        <TouchableOpacity onPress={() => onFacebookButtonPress().then(() => console.log('Signed in with Facebook!'))}>
+        <TouchableOpacity onPress={onFacebookButtonPress}>
           <Image source={require('../assets/images/fb.png')} />
         </TouchableOpacity>  
       </SocialLoginView>
